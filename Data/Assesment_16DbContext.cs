fd437ccd3e7812f58ef6e7729b3761dd6e1db065
Anjali-st <anjali90908080@gmail.com>
2023-09-05 00:30:22 +0530

    Assignment-16

---
+++ Data/Assesment_16DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppAssesment_16.Models;
+
+namespace WebAppAssesment_16.Data
+{
+    public class Assesment_16DbContext : DbContext
+    {
+        public Assesment_16DbContext (DbContextOptions<Assesment_16DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppAssesment_16.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppAssesment_16.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Assesment_16DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Assesment_16DbContext") ?? throw new InvalidOperationException("Connection string 'Assesment_16DbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
